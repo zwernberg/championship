be25b26ebf8a1814008bacbe43ed01a336947e84
Zach Wernberg <zawerny@gmail.com>
2017-12-19 13:07:24 -0600

    sort players

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <h1>Schumacher Championship</h1>
     <h3> Little Minnies  - Neal Schumacher</h3> Projected {{projectedPoints(nealPlayers)}}
     <ul>
-      <li v-for='(player, index) in nealPlayers' :key='index'>
+      <li v-for='(player, index) in sorted(nealPlayers)' :key='index'>
         <b>Name</b>: {{player.player.firstName}}  {{player.player.lastName}}
         <b>Position</b>: {{getPosition(player)}}
         <b>Projected</b>: {{player.currentPeriodProjectedStats.appliedStatTotal}}
@@ -12,7 +12,7 @@
     </ul>
     <h3> Quinley's Mother  - Kensey Schultz </h3> Projected {{projectedPoints(kenseyPlayers)}}
     <ul>
-      <li v-for='(player, index) in kenseyPlayers' :key='index'>
+      <li v-for='(player, index) in sorted(kenseyPlayers)' :key='index'>
         <b>Name</b>: {{player.player.firstName}}  {{player.player.lastName}}
         <b>Position</b>: {{getPosition(player)}}
         <b>Projected</b>: {{player.currentPeriodProjectedStats.appliedStatTotal}}
@@ -23,7 +23,10 @@
 
 <script>
 import axios from 'axios';
-var roster_slots = {0:'QB',1: 'QB', 2:'RB',3:'WR', 4:'WR',6:'TE',23:'FLEX',16:'D/ST',17:'K',20:'Bench'};
+import lodash from 'lodash';
+var _ = lodash;
+
+var roster_slots = {0:'QB',1: 'QB', 2:'RB',3:'WR', 4:'WR',5: 'K', 6:'TE',23:'FLEX',16:'D/ST',17:'K',20:'Bench'};
 
 
 export default {
@@ -38,13 +41,16 @@ export default {
     axios.get('http://games.espn.com/ffl/api/v2/playerInfo?seasonId=2017&leagueId=1477590&useCurrentPeriodRealStats=true&useCurrentPeriodProjectedStats=true&playerId=16724,18218,13981,16800,15072,15839,14198,9354')
     .then(response => {
       this.nealPlayers=response.data.playerInfo.players;
-    })
-        axios.get('http://games.espn.com/ffl/api/v2/playerInfo?seasonId=2017&leagueId=1477590&useCurrentPeriodRealStats=true&useCurrentPeriodProjectedStats=true&playerId=13994,12514,17683,14912,17929,17668,17933,18409')
+    });
+    axios.get('http://games.espn.com/ffl/api/v2/playerInfo?seasonId=2017&leagueId=1477590&useCurrentPeriodRealStats=true&useCurrentPeriodProjectedStats=true&playerId=13994,12514,17683,14912,17929,17668,17933,18409')
     .then(response => {
       this.kenseyPlayers=response.data.playerInfo.players;
-    })
+    });
   },
   methods: {
+    sorted: players => {
+      return _.orderBy(players, 'player.defaultPositionId', 'asc');
+    },
     getPosition: player => {
       return roster_slots[player.player.defaultPositionId];
     },
